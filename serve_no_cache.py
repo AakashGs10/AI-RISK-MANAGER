8263b109252d672e549deceb1f9ace393f2563f1
Aakash G S <gsaakash583@gmail.com>
2026-08-30 11:16:57 +0530

    Force UTF-8 Content-Type in dev server to prevent mojibake

--- serve_no_cache.py
+++ serve_no_cache.py
@@ -10,6 +10,9 @@ class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
     def end_headers(self):
+        # Force UTF-8 encoding so the browser doesn't guess Windows-1252
+        if self.path.endswith(".html"):
+            self.send_header("Content-Type", "text/html; charset=utf-8")
         self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
